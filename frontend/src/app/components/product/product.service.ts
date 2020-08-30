568452d54cfe0059c9a89c355fb1cf9cb02cba0b
Vractos <53495782+Vractos@users.noreply.github.com>
2020-08-30 15:43:05 -0300

    create product (service)

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './product.model'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private snackBar: MatSnackBar ) { }
+  baseUrl = "http://localhost:3001/products"
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient ) { }
 
   showMessage(msg: string): void {
-    this.snackBar.open(msg, 'x', {
+    this.snackBar.open(msg, 'Fechar', {
       duration: 3000,
       horizontalPosition: 'right',
       verticalPosition: 'top'
     })
+  }
+
+  create(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>(this.baseUrl, product)
   }
 }
